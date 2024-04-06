54028398641993085c364534cd3016a785af0bf2
Thomas Hollier <thomas@RPair01.local>
2024-04-05 23:46:29 -0700

    Renders both the version with lines and the clean version, along with the tile

--- wallpaper_groups_image_production/production_with_line.py
+++ wallpaper_groups_image_production/production_with_line.py
@@ -2,100 +2,105 @@ from base.seventeen_wallpaper_line import *
 
 
 def draw_line(l, m, deg, source_image_path, save_image_path, is_show):
-    # 生成带标识的图
-    info3 = [l, m, deg, 3]  # 平行四边形
+
+    # Generate the image with identifiers
+    info1 = [l, 1]  # Square
+    point1 = Point(info=info1).run()
+
+    info2 = [l, m, 2]  # Rectangle
+    point2 = Point(info=info2).run()
+
+    info3 = [l, m, deg, 3]  # Parallelogram
     point3 = Point(info=info3).run()
 
-    info4 = [l, 60, 4]  # 菱形
+    info4 = [l, 60, 4]  # Rhombus
     point4 = Point(info=info4).run()
 
-    info4_3 = [l, l, 60, 3]  # 模仿菱形的平行四边形
+    info4_3 = [l, l, 60, 3]  # Parallelogram mimicking rhombus
     point4_3 = Point(info=info4_3).run()
 
-    info5 = [l, 5]  # 正六边形
+    info5 = [l, 5]  # Hexagon
     point5 = Point(info=info5).run()
 
     y = int(l * np.cos(30 * np.pi / 180)) * 2
-    info6_1 = [y, l, 30, 6]  # 等腰30度三角形
+    info6_1 = [y, l, 30, 6]  # Isosceles triangle with 30 degrees angle
     point6_1 = Point(info=info6_1).run()
 
-    info6_1_2 = [l, l, 90, 6]  # 等腰直角三角形
+    info6_1_2 = [l, l, 90, 6]  # Right isosceles triangle
     point6_1_2 = Point(info=info6_1_2).run()
 
     x = int(l * np.cos(deg * np.pi / 180))
-    info6_2 = [x, l, deg, 6]  # 直角三角形
+    info6_2 = [x, l, deg, 6]  # Right triangle
     point6_2 = Point(info=info6_2).run()
 
     x = int(l * np.cos(45 * np.pi / 180)) * 2
-    info6_2_1 = [x, l, 45, 6]  # 45度等腰直角三角形
+    info6_2_1 = [x, l, 45, 6]  # 45-degree isosceles right triangle
     point6_2_1 = Point(info=info6_2_1).run()
 
     x = int(l * np.cos(60 * np.pi / 180))
-    info6_2_2 = [x, l, 60, 6]  # 直角60三角形
+    info6_2_2 = [x, l, 60, 6]  # Right 60 triangle
     point6_2_2 = Point(info=info6_2_2).run()
 
-    print('random value:', l, m, deg)
-
-    info6_3 = [l, l, 60, 6]  # 等边三角形
+    info6_3 = [l, l, 60, 6]  # Equilateral triangle
     point6_3 = Point(info=info6_3).run()
 
-    info1 = [l, 1]  # 正方形
-    point1 = Point(info=info1).run()
-    info2 = [l, m, 2]  # 长方形
-    point2 = Point(info=info2).run()
 
     kwargs = {'source_image_path': source_image_path, 'save_image_path': save_image_path, 'is_show': is_show,
               'picture_width': width, 'picture_height': height}
 
-    P1Line(point=point1, info=info1, **kwargs).generate()
-    P1Line(point=point2, info=info2, **kwargs).generate()
-    P1Line(point=point3, info=info3, **kwargs).generate()
-    P1Line(point=point4, info=info4, **kwargs).generate()
-    P1Line(point=point5, info=info5, **kwargs).generate()
+    groups = {
+            'P1_1':'P1(point=point1, info=info1, **kwargs).generate()',
+            'P1-2':'P1(point=point2, info=info2, **kwargs).generate()',
+            'P1-3':'P1(point=point3, info=info3, **kwargs).generate()',
+            'P1-4':'P1(point=point4, info=info4, **kwargs).generate()',
+            'P1-5':'P1(point=point5, info=info5, **kwargs).generate()',
+            'P2-1':'P2(point=point2, info=info2, **kwargs).generate()',
+            'P2-2':'P2(point=point3, info=info3, **kwargs).generate()',
+            'P2-3':'P2(point=point6_1, info=info6_1, **kwargs).generate()',
+            'P2Hexagonal':'P2Hexagonal(point=point6_3, info=info6_3, **kwargs).generate()',
+            'Pm-1':'Pm(point=point1, info=info1, **kwargs).generate()',
+            'Pm-2':'Pm(point=point2, info=info2, **kwargs).generate()',
+            'Pg':'Pg(point=point2, info=info2, **kwargs).generate()',
+            'Cm':'Cm(point=point6_1, info=info6_1, **kwargs).generate()',
+            'Pmm-1':'Pmm(point=point1, info=info1, **kwargs).generate()',
+            'Pmm-2':'Pmm(point=point2, info=info2, **kwargs).generate()',
+            'Pmg':'Pmg(point=point2, info=info2, **kwargs).generate()',
+            'Pgg':'Pgg(point=point6_1, info=info6_1, **kwargs).generate()',
+            'Cmm-1':'Cmm(point=point6_2, info=info6_2, type="rhombic", **kwargs).generate()',
+            'Cmm-2':'Cmm(point=point6_2_1, info=info6_2_1, type="square", **kwargs).generate()',
+            'P4':'P4(point=point1, info=info1, **kwargs).generate()',
+            'P4m':'P4m(point=point6_1_2, info=info6_1_2, **kwargs).generate()',
+            'P4g':'P4g(point=point6_1_2, info=info6_1_2, **kwargs).generate()',
+            'P3':'P3(point=point4_3, info=info4_3, **kwargs).generate()',
+            'P3m1':'P3m1(point=point6_3, info=info6_3, **kwargs).generate()',
+            'P31m':'P31m(point=point6_1, info=info6_1, **kwargs).generate()',
+            'P6':'P6(point=point6_3, info=info6_3, **kwargs).generate()',
+            'P6m':'P6m(point=point6_2_2, info=info6_2_2, **kwargs).generate()'
+    }
 
-    P2Line(point=point2, info=info2, **kwargs).generate()
-    P2Line(point=point3, info=info3, **kwargs).generate()
-    P2Line(point=point6_1, info=info6_1, **kwargs).generate()
-    P2HexagonalLine(point=point6_3, info=info6_3, **kwargs).generate()
+    for k, v in groups.items():
+        eval(v)
+        eval(v.replace("(point=","Line(point="))
 
-    PmLine(point=point1, info=info1, **kwargs).generate()
-    PmLine(point=point2, info=info2, **kwargs).generate()
-    PgLine(point=point2, info=info2, **kwargs).generate()
-    CmLine(point=point6_1, info=info6_1, **kwargs).generate()
-    PmmLine(point=point1, info=info1, **kwargs).generate()
-    PmmLine(point=point2, info=info2, **kwargs).generate()
-    PmgLine(point=point2, info=info2, **kwargs).generate()
-    PggLine(point=point6_1, info=info6_1, **kwargs).generate()
 
-    CmmLine(point=point6_2, info=info6_2, **kwargs).generate()
-    CmmSquareLine(point=point6_2_1, info=info6_2_1, **kwargs).generate()
 
-    P4Line(point=point1, info=info1, **kwargs).generate()
-    P4mLine(point=point6_1_2, info=info6_1_2, **kwargs).generate()
-    P4gLine(point=point6_1_2, info=info6_1_2, **kwargs).generate()
-
-    P3Line(point=point4_3, info=info4_3, **kwargs).generate()
-    P3m1Line(point=point6_3, info=info6_3, **kwargs).generate()
-    P31mLine(point=point6_1, info=info6_1, **kwargs).generate()
-
-    P6Line(point=point6_3, info=info6_3, **kwargs).generate()
-    P6mLine(point=point6_2_2, info=info6_2_2, **kwargs).generate()
 
 
 if __name__ == '__main__':
     """
-        source_image_path 原图片  save_folder 保存文件目录
+        source_image_path: original image
+        save_folder: save directory
     """
-    source_image_path = '../images/ori_img/shao.png'
-    save_folder = '../images/output_with_line'
+    source_image_path = '../../Images/basic-L.jpg'
+    save_folder = '../../Outputs'
 
-    # 最后呈现的画布大小
-    width, height = 800, 800
+    # Final canvas size
+    width, height = 1024, 1024
 
-    # cell：两边长一夹角（l,m,deg）
-    l = 100
-    m = int(l * 0.618)
-    deg = 60
+    # Parameters for the line: (l, m, deg)
+    l = 160
+    m = 160
+    deg = 40
 
-    # is_show: 是否在运行中展示
+    # is_show: Whether to display while running
     draw_line(l, m, deg, source_image_path, save_folder, is_show=False)
